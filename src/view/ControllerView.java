faabc4c2b6e064762fdc1b36c8a7dabfef944a15
oriKanner <orikanner12345@gmail.com>
2023-01-07 17:39:24 +0200

    buttons view & controller

--- src/view/ControllerView.java
+++ src/view/ControllerView.java
@@ -7,12 +7,16 @@ import javafx.scene.layout.Pane;
 import java.util.Observable;
 import java.util.Observer;
 import view.views.Joystic.Joystick;
+import view.views.btn.PlayerButtons;
 
 public class ControllerView extends Pane implements Observer {
 
 
     @FXML
     Joystick Joystick;
+
+    @FXML
+    PlayerButtons playerButtons;
 
 
     void init() {
