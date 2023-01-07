8c03400d5f9d8d045db9c1006a37e0fe18c898fa
Isar_NS <thxr0654@gmail.com>
2023-01-07 19:26:15 +0200

    fix conflicts

--- src/view/ControllerView.java
+++ src/view/ControllerView.java
@@ -6,13 +6,9 @@ import javafx.scene.layout.Pane;
 import java.util.Observable;
 import java.util.Observer;
 import view.views.Joystic.Joystick;
-<<<<<<< HEAD
 import view.views.Gauges.Gauges;
-=======
 import view.views.Buttons.PlayerButtons;
-import view.views.Gauges.Gauges;
 
->>>>>>> 9f180516f63d0641f199a9c7b069ad7376c9a304
 
 public class ControllerView extends Pane implements Observer {
 
@@ -21,14 +17,10 @@ public class ControllerView extends Pane implements Observer {
     Joystick Joystick;
 
     @FXML
-<<<<<<< HEAD
-    Gauges timeBoard;
-=======
     PlayerButtons playerButtons;
 
     @FXML
     Gauges gauges;
->>>>>>> 9f180516f63d0641f199a9c7b069ad7376c9a304
 
 
     void init() {
