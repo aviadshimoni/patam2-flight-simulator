84ffa1ffca2e11cda390ec43ee755122ba406e59
oriKanner <orikanner12345@gmail.com>
2023-01-08 20:17:55 +0200

    Attributes List

---
+++ src/view/views/Attributes/Attributes.java
@@ -0,0 +1,32 @@
+package view.views.Attributes;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+public class Attributes extends AnchorPane {
+
+    public ObservableList list;
+    public StringProperty chosenAttribute;
+    public AttributesController attributesController;
+
+    public Attributes() {
+        super();
+        FXMLLoader Loader = new FXMLLoader((getClass().getResource("Attributes.fxml")));
+        list = FXCollections.observableArrayList();
+        chosenAttribute = new SimpleStringProperty();
+        try {
+            AnchorPane list = Loader.load();
+            attributesController = Loader.getController();
+            chosenAttribute.bind(attributesController.listView.getSelectionModel().selectedItemProperty());
+
+            this.getChildren().add(list);
+        } catch (IOException e) {
+        }
+    }
+}
---
+++ src/view/views/Attributes/AttributesController.java
@@ -0,0 +1,11 @@
+package view.views.Attributes;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class AttributesController {
+
+    @FXML
+   public ListView listView;
+
+}
